193eead4257703d90c47bc5654c4a970656e7065
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2023-11-07 09:32:12 +0100

    Update 07_11_2023.sql

--- PhpMyAdmin/07_11_2023.sql
+++ PhpMyAdmin/07_11_2023.sql
@@ -30,6 +30,30 @@
 -- Dodaj klienta
     INSERT INTO klienci SET idklienta=NULL, imie="Steve", nazwisko="McQueen", miejscowosc="Los Angeles"
 
+-- ZADANIA
+-- 1.
+    SELECT tytul FROM ksiazki ORDER BY tytul
+-- 2.
+    SELECT tytul, cena FROM ksiazki ORDER BY cena DESC LIMIT 1
+-- 3. 
+    SELECT * FROM zamowienia WHERE zamowienia.status = "wyslano"
+-- 4. 
+    SELECT imie, nazwisko FROM klienci WHERE klienci.nazwisko = "Rutkowski"
+-- 5. 
+    SELECT tytul FROM ksiazki WHERE ksiazki.tytul LIKE "%PHP%"
+-- 6. 
+    SELECT * FROM zamowienia ORDER BY zamowienia.data DESC
+-- 7. 
+    SELECT nazwisko, imie, idzamowienia, data FROM zamowienia, klienci WHERE zamowienia.idklienta = klienci.idklienta
+-- 7a.
+    SELECT nazwisko AS "Nazwisko", imie AS "Imię", idzamowienia AS "ID zamówienia", data AS "Data zamówienia" FROM zamowienia, klienci WHERE zamowienia.idklienta = klienci.idklienta
+-- 8.
+    SELECT imie, nazwisko FROM klienci, zamowienia WHERE klienci.idklienta = zamowienia.idklienta AND idksiazki = 2
+-- 9. 
+    SELECT tytul, nazwiskoautora FROM zamowienia, klienci, ksiazki WHERE zamowienia.idklienta = klienci.idklienta AND zamowienia.idksiazki = ksiazki.idksiazki AND klienci.imie = "Jan" AND klienci.nazwisko = "Nowak"
+-- 10.
+    SELECT nazwisko, imie, idzamowienia, data, status, tytul FROM zamowienia, klienci, ksiazki WHERE zamowienia.idklienta = klienci.idklienta AND zamowienia.idksiazki = ksiazki.idksiazki AND klienci.nazwisko LIKE "Rutkowski" ORDER BY data
+
 
 
 
