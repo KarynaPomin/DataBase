8552dc00ef3b0156884ae0f3b0a14b792bf06e13
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-01-16 09:22:14 +0100

    Create Podzapytania.sql
    no

---
+++ KLASA_2/Podzapytania.sql
@@ -0,0 +1,22 @@
+-- 1. Wyświetl wszystkie  zamówienia wysłane 3 października 2017 roku i nazwiska klientów, którzy je złożyli.
+
+SELECT klienci.NazwiskoKlienta, klienci.ImieKlienta, zamowienia.DataZamowienia, zamowienia.DataWysylki FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta WHERE zamowienia.DataWysylki LIKE '2017-10-03'
+ 
+-- 2. Wymień wszystkich klientów i liczbę zamówień złożonych przez każdego z nich.
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, COUNT(zamowienia.NumerZamowienia) AS Liczbazamowien FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta GROUP BY klienci.NazwiskoKlienta;
+
+-- Podzapytanie
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta,
+	(SELECT COUNT(*)
+    FROM zamowienia
+    WHERE zamowienia.IDKlienta=klienci.IDKlienta) AS LiczZam
+FROM klienci;
+
+
+-- 3. Podaj listę klientów i daty ostatniego złożonego przez nich zamówienia
+
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta,
+	(SELECT MAX(zamowienia.DataZamowienia)
+	FROM zamowienia
+    WHERE klienci.IDKlienta=zamowienia.IDKlienta) AS DataOstZam
+FROM klienci
