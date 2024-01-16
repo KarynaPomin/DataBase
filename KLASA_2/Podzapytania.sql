e9a01ed6498ded838abb34e132985f6cd377543b
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-01-16 09:33:24 +0100

    Update Podzapytania.sql
    yeeee

--- KLASA_2/Podzapytania.sql
+++ KLASA_2/Podzapytania.sql
@@ -1,9 +1,13 @@
 -- 1. Wyświetl wszystkie  zamówienia wysłane 3 października 2017 roku i nazwiska klientów, którzy je złożyli.
 
-SELECT klienci.NazwiskoKlienta, klienci.ImieKlienta, zamowienia.DataZamowienia, zamowienia.DataWysylki FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta WHERE zamowienia.DataWysylki LIKE '2017-10-03'
+SELECT klienci.NazwiskoKlienta, klienci.ImieKlienta, zamowienia.DataZamowienia, zamowienia.DataWysylki 
+FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta 
+WHERE zamowienia.DataWysylki LIKE '2017-10-03'
  
 -- 2. Wymień wszystkich klientów i liczbę zamówień złożonych przez każdego z nich.
-SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, COUNT(zamowienia.NumerZamowienia) AS Liczbazamowien FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta GROUP BY klienci.NazwiskoKlienta;
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, COUNT(zamowienia.NumerZamowienia) AS Liczbazamowien 
+FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta
+GROUP BY klienci.NazwiskoKlienta;
 
 -- Podzapytanie
 SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta,
@@ -22,4 +26,9 @@ SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta,
 FROM klienci
 
 -- 4. Zrób listę klientów i wszystkich szczegółów dotyczących ich ostatniego zamówienia.
-SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, zamowienia.NumerZamowienia, zamowienia.DataZamowienia, zamowienia.DataWysylki, (SELECT MAX(zamowienia.DataZamowienia) FROM zamowienia WHERE klienci.IDKlienta=zamowienia.IDKlienta) AS DataOstZam FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta GROUP BY klienci.NazwiskoKlienta;
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, zamowienia.NumerZamowienia, zamowienia.DataZamowienia, zamowienia.DataWysylki,
+	(SELECT MAX(zamowienia.DataZamowienia)
+	FROM zamowienia 
+	WHERE klienci.IDKlienta=zamowienia.IDKlienta) AS DataOstZam
+FROM klienci INNER join zamowienia ON klienci.IDKlienta=zamowienia.IDKlienta 
+GROUP BY klienci.NazwiskoKlienta;
