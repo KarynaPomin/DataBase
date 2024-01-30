f963c4ef326068c5cc56cccd06571aa930a25068
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-01-30 08:34:49 +0100

    Create IsNull.sql

---
+++ KLASA_2/IsNull.sql
@@ -0,0 +1,15 @@
+-- Wymień składniki nieużywane dotąd w żadnym przepisie (Baza danych: PrzepisyPrzyklad)
+
+-- SPOSÓB I
+SELECT skladniki.NazwaSkladnika
+FROM skladniki 
+WHERE skladniki.IDSkladnika NOT IN (SELECT skladniki_przepisow.IDSkladnika FROM skladniki_przepisow);
+
+-- SPOSÓB II
+SELECT skladniki.NazwaSkladnika
+FROM skladniki 
+LEFT OUTER JOIN skladniki_przepisow
+ON skladniki.IDSkladnika=skladniki_przepisow.IDSkladnika
+WHERE skladniki_przepisow.IDPrzepisu IS NULL;
+
+--
