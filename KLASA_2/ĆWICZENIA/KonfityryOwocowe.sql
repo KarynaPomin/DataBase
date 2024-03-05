e9a6ad92b076c9092d0e83d3ee26c2fb336f30b6
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-03-05 16:41:06 +0100

    Create KonfityryOwocowe.sql

---
+++ KLASA_2/ĆWICZENIA/KonfityryOwocowe.sql
@@ -0,0 +1,8 @@
+
+### ZADANIE 1
+# Dla każdego miesiąca pracy przetwórni (od maja do września) wykonaj zestawienie liczby 
+# dostarczonych kilogramów malin, liczby dostarczonych kilogramów truskawek i liczby 
+# dostarczonych kilogramów porzeczek. 
+# Na podstawie wykonanego zestawienia utwórz wykres kolumnowy. Pamiętaj o czytelnym 
+# opisie wykresu (tytuł, legenda, opisy osi: na osi X – nazwy miesięcy, na osi Y – liczba 
+# kilogramów).
