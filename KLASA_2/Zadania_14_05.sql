f641647759670c50a6b961340627e80badd68849
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-05-14 09:07:12 +0200

    Update Zadania_14_05.sql

--- KLASA_2/Zadania_14_05.sql
+++ KLASA_2/Zadania_14_05.sql
@@ -5,3 +5,18 @@ UNION
 SELECT wykonawcy.NazwaScenicznaWykonawcy AS Personalia, 'Wykonawca' AS Typ
 FROM wykonawcy
 ORDER BY Personalia DESC;
+
+# Wyszukaj wykonawców, którzy występowali dla klientów o nazwiskach Berg albo Hallmark. 
+SELECT DISTINCT wykonawcy.NazwaScenicznaWykonawcy
+FROM wykonawcy, klienci, imprezy
+WHERE wykonawcy.IDWykonawcy = imprezy.IDWykonawcy AND imprezy.IDKlienta = klienci.IDKlienta
+AND (klienci.NazwiskoKlienta LIKE "Berg" OR klienci.NazwiskoKlienta LIKE "Hallmark");
+
+# Podaj przepisy na dania główne wraz ze wszystkimi składnikami. Otrzymane wyniki posortuj wg NazwyPrzepisu alfabetycznie. Przepisy
+SELECT przepisy.NazwaPrzepisu, skladniki.NazwaSkladnika, miary.OpisMiary, skladniki_przepisow.Ilosc
+FROM przepisy, skladniki_przepisow, skladniki, kategorie_przepisow, miary
+WHERE przepisy.IDPrzepisu = skladniki_przepisow.IDPrzepisu 
+AND skladniki_przepisow.IDSkladnika = skladniki.IDSkladnika 
+AND miary.IDMiaryIlosci = skladniki_przepisow.IDMiaryIlosci
+AND kategorie_przepisow.OpisKategoriiPrzepisu LIKE "dania g%"
+ORDER BY przepisy.NazwaPrzepisu;
